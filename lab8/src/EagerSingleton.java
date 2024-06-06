80b49858e0b781777b9e7a3981463dcef40a22ec
ogurecze <162505048+ogurecze@users.noreply.github.com>
2024-06-06 11:58:44 +0200

    Delete lab8 directory

--- lab8/src/EagerSingleton.java
+++
@@ -1,9 +0,0 @@
-public class EagerSingleton {
-    private static EagerSingleton instance = new EagerSingleton();
-    private EagerSingleton(){
-
-    }
-    public static EagerSingleton getInstance(){
-        return instance;
-    }
-}
--- lab8/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Cześć");
-        LazySingleton s1 = LazySingleton.getInstance();
-        LazySingleton s2 = LazySingleton.getInstance();
-
-        System.out.println(s1 + " " + s2);
-    }
-}
